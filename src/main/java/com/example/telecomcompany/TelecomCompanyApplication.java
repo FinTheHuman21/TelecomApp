6b29e25382ac3cd7772d1cb4d6b24fa50f56e988
Finth Humanul <rdsdragos3@gmail.com>
2022-12-13 17:54:12 +0200

    initial commit

---
+++ src/main/java/com/example/telecomcompany/TelecomCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.telecomcompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelecomCompanyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/telecomcompany/TelecomCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telecomcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelecomCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
